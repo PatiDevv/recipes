d5fb3878b942e94c735eb88aa957c95ef43a98e3
PatiDevv <kubinskapatrycja@gmail.com>
2021-07-07 22:18:15 +0200

    Add styled components, background for main page.

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,12 +1,27 @@
+import styled from "styled-components";
+import background from "../images/background.jpg";
+
+const StyledDiv = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0)), url(${background});
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+`;
+
+const StyledH1 = styled.h1`
+  color: #fff;
+  font-size: 45px;
+  font-family: sans-serif;
+  top: 30%;
+  left: 15%;
+  position: absolute;
+`;
+
 const HomePage = () => {
   return (
-    <>
-      <h1>Strona Główna</h1>
-      <h1>Strona Główna</h1>
-      <h1>Strona Główna</h1>
-      <h1>Strona Główna</h1>
-      <h1>Strona Główna</h1>
-    </>
+    <StyledDiv>
+      <StyledH1>Healthy Recipes for every day</StyledH1>
+    </StyledDiv>
   );
 };
 
