75ead595388edeabee65d390ede1d65ca501def5
PatiDevv <kubinskapatrycja@gmail.com>
2021-07-12 16:21:04 +0200

    Add view search engine.

---
+++ src/components/atoms/Input/SearchInput.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import magnifierIcon from "../../../images/magnifier.svg";
+
+const SearchInput = styled.input`
+  border: none;
+  border-radius: 50px;
+
+  :focus-visible {
+    outline: none;
+  }
+
+  ::placeholder {
+    letter-spacing: 2px;
+    color: #727272;
+  }
+
+  padding: 10px 20px 10px 40px;
+
+  background-image: url(${magnifierIcon});
+  background-size: 15px;
+  background-repeat: no-repeat;
+  background-position: 15px 50%;
+`;
+
+export default SearchInput;
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,16 +1,31 @@
 import styled from "styled-components";
+import SearchInput from "../components/atoms/Input/SearchInput";
 import background from "../images/background.jpg";
 
-const StyledDiv = styled.div`
+const StyledWrap = styled.div`
   background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0)), url(${background});
   background-size: 200%;
   background-position: center;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
-  align-content: center;
+  position: relative;
+`;
+
+const MainText = styled.div`
+  flex: 90%;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+`;
+
+const StyedSearchInput = styled(SearchInput)`
+  margin: 1%;
+  width: 50%;
+  position: absolute;
+  left: 10%;
+  top: 2%;
 `;
 
 const StyledH1 = styled.h1`
@@ -18,7 +33,7 @@ const StyledH1 = styled.h1`
   font-size: 400%;
   line-height: 0.9;
   color: #1c2025;
-  margin: 15px 0;
+  margin: 20px 0;
   text-align: center;
 `;
 
@@ -29,22 +44,25 @@ const StyledH2 = styled.h2`
   font-size: 140%;
   line-height: 1.2;
   color: #1c2025;
-  margin: 15px 0;
+  margin: 20px 0;
   text-align: center;
 `;
 
 const HomePage = () => {
   return (
-    <StyledDiv>
-      <StyledH1>
-        Healthy Recipes <br />
-        for every day
-      </StyledH1>
-      <StyledH2>
-        Eat delicious and healthy every day. In our database you will find <strong>10,000</strong> recipes. Ingredients, proportions, shopping list on one website.{" "}
-        <strong>We are there for you.</strong>
-      </StyledH2>
-    </StyledDiv>
+    <StyledWrap>
+      <StyedSearchInput placeholder="Find Me - Your Perfect Recipe for Today"></StyedSearchInput>
+      <MainText>
+        <StyledH1>
+          Healthy Recipes <br />
+          for every day
+        </StyledH1>
+        <StyledH2>
+          Eat delicious and healthy every day. In our database you will find <strong>10,000</strong> recipes. Ingredients, proportions, shopping list on one website.{" "}
+          <strong>We are there for you.</strong>
+        </StyledH2>
+      </MainText>
+    </StyledWrap>
   );
 };
 
