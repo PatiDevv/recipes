880cb202ef2d79c07e5ef0ecffdd794bc4d89897
PatiDevv <kubinskapatrycja@gmail.com>
2021-08-10 16:17:00 +0200

    Animation has added to CardRecipe.

--- src/components/organisms/RecipeCard.tsx
+++ src/components/organisms/RecipeCard.tsx
@@ -2,10 +2,8 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { FakeDataItem } from '../../redux/global';
 import { getRoute } from '../../routes';
-import ImageAnimatedSplitView from '../atoms/ImageAnimatedSplitView';
 
 const StyledWrapper = styled.div`
-  background-color: #60cf98;
   height: 40%;
   width: 400px;
   border-radius: 30px;
@@ -14,13 +12,16 @@ const StyledWrapper = styled.div`
   margin: 1%;
   cursor: pointer;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
-`;
 
-const ImageWrapper = styled.img`
-  border-radius: 30px 30px 0 0;
-  height: 300px;
-  width: 400px;
-  object-fit: cover;
+  position: relative;
+  overflow: hidden;
+
+  * {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-transition: all 0.35s ease;
+    transition: all 0.35s ease;
+  }
 `;
 
 const StyledTable = styled.div`
@@ -38,7 +39,10 @@ const Header = styled.header`
   font-size: 20px;
   font-weight: 600;
   text-align: center;
-  padding: 13px;
+  padding: 13px 0 0 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const H3Button = styled.h3`
@@ -64,9 +68,48 @@ const H3Top = styled.h3`
 const Line = styled.hr`
   width: 30%;
   margin: 0 35%;
+  border: none;
+  border-radius: 100%;
+  background-color: #fff;
+  height: 1px;
+  margin-top: 5px;
 `;
 
-interface RecipeCardProps extends FakeDataItem {
+const ImageWrapper = styled.img`
+  border-radius: 30px 30px 0 0;
+  height: 300px;
+  width: 400px;
+  object-fit: cover;
+  max-width: 100%;
+  vertical-align: top;
+`;
+
+const Figcaption = styled.figcaption`
+  position: absolute;
+  top: calc(80%);
+  width: 100%;
+
+  background-color: #60cf98;
+  padding: 0 10px 100px 10px;
+
+  :before {
+    position: absolute;
+    content: '';
+    z-index: 2;
+    bottom: 100%;
+    left: 0;
+    width: 100%;
+    height: 10px;
+
+    background-image: linear-gradient(to bottom, transparent 0%, #60cf98 100%);
+  }
+
+  :hover {
+    top: 63%;
+  }
+`;
+
+export interface RecipeCardProps extends FakeDataItem {
   time: number;
 }
 
@@ -76,21 +119,22 @@ const RecipeCard = ({ name, calories, protein, carbs, fat, time, image, id }: Re
   return (
     <StyledWrapper onClick={() => history.push(getRoute.recipe(id))}>
       <ImageWrapper src={image} alt="Jedzenie" />
-
-      <Header>{name.length > 35 ? name.substring(0, 35) + '...' : name}</Header>
-      <Line />
-      <StyledTable>
-        <H3Button>Calories</H3Button>
-        <H3Button>Protein</H3Button>
-        <H3Button>Carbs</H3Button>
-        <H3Button>Fats</H3Button>
-        <H3Button>Time</H3Button>
-        <H3Top>{calories}</H3Top>
-        <H3Top>{protein}</H3Top>
-        <H3Top>{carbs}</H3Top>
-        <H3Top>{fat}</H3Top>
-        <H3Top>{time}'</H3Top>
-      </StyledTable>
+      <Figcaption>
+        <Header>{name}</Header>
+        <Line />
+        <StyledTable>
+          <H3Button>Calories</H3Button>
+          <H3Button>Protein</H3Button>
+          <H3Button>Carbs</H3Button>
+          <H3Button>Fats</H3Button>
+          <H3Button>Time</H3Button>
+          <H3Top>{calories}</H3Top>
+          <H3Top>{protein}</H3Top>
+          <H3Top>{carbs}</H3Top>
+          <H3Top>{fat}</H3Top>
+          <H3Top>{time}'</H3Top>
+        </StyledTable>
+      </Figcaption>
     </StyledWrapper>
   );
 };
